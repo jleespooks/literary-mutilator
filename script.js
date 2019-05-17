26a84e1a41fb2386849ac8bd15d6bba26704a2e0
Jamie Amparan <jleespooks@gmail.com>
2019-05-17 02:00:33 -0600

    text will change from bird to meow

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//went in on the styling here. Very fun!
 function tier1Challenge(){
 	document.getElementById("one").style.backgroundColor = "purple";
 	document.getElementById("one").style.color = "Pink";
@@ -16,4 +17,11 @@ function tier1challengefun(){
 	document.getElementById("three").style.fontFamily = "courier new";
 	document.getElementById("three").style.fontWeight = "italic";
 
+}
+
+// every 'bird' in cat ipsum will be replaced with every click
+function replaceAnd() {
+	let catPara = document.getElementById("two");
+	let regex = /bird/;
+	catPara.innerHTML = catPara.innerHTML.replace(regex, 'meeeeeooooowww')
 }
