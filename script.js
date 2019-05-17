06e93fc508ddb4dd9235d0ba7ee0106c35c5f8ad
Jamie Amparan <jleespooks@gmail.com>
2019-05-17 01:35:53 -0600

    added color to second p tag

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function tier1Challenge(){
 	document.getElementById("one").style.fontFamily= "impact";
 }
 
+function tier1challengePlus(){
+	document.getElementById("two").style.backgroundColor ="brown";
+	document.getElementById("two").style.color= "yellow";
+	document.getElementById("two").style.fontFamily= "comic sans MS";
+}
