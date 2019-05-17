dd8454583c808147b0f89471c30802702b1cc100
Jamie Amparan <jleespooks@gmail.com>
2019-05-17 01:46:27 -0600

    extra credit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function tier1Challenge(){
-	document.getElementById("one").style.backgroundColor ="purple";
-	document.getElementById("one").style.color= "Pink";
-	document.getElementById("one").style.fontFamily= "impact";
+	document.getElementById("one").style.backgroundColor = "purple";
+	document.getElementById("one").style.color = "Pink";
+	document.getElementById("one").style.fontFamily = "impact";
 }
 
 function tier1challengePlus(){
-	document.getElementById("two").style.backgroundColor ="brown";
-	document.getElementById("two").style.color= "yellow";
-	document.getElementById("two").style.fontFamily= "comic sans MS";
+	document.getElementById("two").style.backgroundColor = "brown";
+	document.getElementById("two").style.color = "yellow";
+	document.getElementById("two").style.fontFamily = "comic sans MS";
+}
+
+function tier1challengefun(){
+	document.getElementById("three").style.backgroundColor = "aqua";
+	document.getElementById("three").style.color = "black";
+	document.getElementById("three").style.fontFamily = "courier new";
+	document.getElementById("three").style.fontWeight = "italic";
+
 }
