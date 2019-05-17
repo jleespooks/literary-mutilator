68f3351961161e11fe1d662af954b18542c9b953
Jamie Amparan <jleespooks@gmail.com>
2019-05-17 01:23:25 -0600

    added more to pretty kitty

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function tier1Challenge(){
+	document.getElementById("one").style.backgroundColor ="purple";
 	document.getElementById("one").style.color= "Pink";
 	document.getElementById("one").style.fontFamily= "impact";
 }
+
